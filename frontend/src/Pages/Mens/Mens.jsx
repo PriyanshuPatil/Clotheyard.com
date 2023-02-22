1f0171fe54fe8ab3b6ff3e89f6020e6012635ac2
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2023-02-22 12:16:05 +0530

    adding things mens page and css and routes

---
+++ frontend/src/Pages/Mens/Mens.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Mens.module.css";
+
+const Mens = () => {
+  const Mensdata = () => {};
+
+  return <div className={styles.main}>Mens</div>;
+};
+
+export default Mens;
